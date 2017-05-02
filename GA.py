790de756c9e8c1bfd3c94d1a4a132f38efca9d38
Vadim <vadimborisov1@gmail.com>
2017-05-02 11:27:30 +0200

    add comments, plus test

--- GA.py
+++ GA.py
@@ -1,23 +1,40 @@
 import datetime
-import random
 import numpy as np
 from eva import EvA
 from random import randint
+import timy
 
 # test target
 target = [1, 1, 1, 2, 2, 2, 3, 3, 3]
 
+# timy decorator for time measurement
+@timy.timer(ident='GA_CC_fun')
+def GA_CC_fun(target, shape):
+    '''
+    (list, tuple) -> list
+    
+    Generic Algorithm for the Common-Centroid optimization 
+    
+    :param target: a list
+    :param shape: a tuple or a list with shape 
+    :return: a list
+    '''
 
-def GA_CC_fun(target, shape_):
-    def generate_parent(length):
-        random.shuffle(length, random.random)
-        return length
+    def get_fitness(guess, shape):
+        '''
+        :param guess: 
+        :param shape: 
+        :return: 
+        '''
 
-    def get_fitness(guess, shape_):
-
-        return (EvA(np.array(guess), shape_))
+        return (EvA(np.array(guess), shape))
 
     def mutate(parent):
+        '''
+        
+        :param parent: 
+        :return: 
+        '''
         first = randint(0, len(parent) - 1)
         second = randint(0, len(parent) - 1)
         while first == second:
@@ -26,41 +43,51 @@ def GA_CC_fun(target, shape_):
         return parent
 
     def display(guess):
+        '''
+        
+        :param guess: 
+        :return: 
+        '''
         timeDiff = datetime.datetime.now() - startTime
-        fitness = get_fitness(guess, shape_)
+        fitness = get_fitness(guess, shape)
         print("{0}\t{1}\t{2}".format(guess, fitness, str(timeDiff)))
 
     startTime = datetime.datetime.now()
 
-    bestParent = generate_parent(target)
-    bestFitness = 10
+    # best Parent is a target in the beginning
+    bestparent = target
+
+    # check initial TMO coefficient
+    bestFitness = EvA(np.array(target), shape)
+
     print("Array \t\t\t\t\t\t\t\t\t\t\t Offset \t time")
 
-    display(bestParent)
+    display(bestparent)
 
-    NNN = 0
-    NumberOfIrr = 0
+    numberofgenerations = 0
+
+    # create a stop criteria
+    number_of_irr = 0
 
     # TODO optimize bellow
-    while True and NumberOfIrr != 40000:
-        NumberOfIrr += 1
-        child = mutate(bestParent)
-        NNN += 1
-        # print("child",child)
-        childFitness = get_fitness(child, shape_)
+    while True and number_of_irr != 40000:
+        number_of_irr += 1
+        child = mutate(bestparent)
+        numberofgenerations += 1
+        childFitness = get_fitness(child, shape)
         if bestFitness <= childFitness:
             continue
         display(child)
         if childFitness < 1:
             break
         bestFitness = childFitness
-        bestParent = child
+        bestparent = child
 
-    print("Number of generations:", NNN)
+    print("Number of generations:", numberofgenerations)
 
-    bestParent = np.array(bestParent)
+    bestparent = np.array(bestparent)
 
-    return bestParent
+    return bestparent
 
 
 print(GA_CC_fun(target, (3, 3)))
